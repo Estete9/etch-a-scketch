1d6d507e811323bcb3611ad53dcbf7d7970a093b
Esteban <cepalacios@outlook.com>
2022-05-25 10:05:23 -0500

    Implement new grid created with user input sizes
    Implemented a function that asks the user to input rows and columns for
    a custom grid.
    It checks if the input is a number and then calls the createGrid
    function with the custom input.

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 let divContent = document.querySelector(".content");
 createGrid();
-mouseOver();
+let newGridBtn = document.querySelector("#newGrid");
+newGridBtn.addEventListener("click", newGrid);
 
-function createGrid() {
-  for (i = 0; i < 16; i++) {
+function createGrid(x = 16) {
+  for (i = 0; i < x; i++) {
     let divC = document.createElement("div");
     divC.style.flexGrow = "1";
     divC.style.display = "flex";
-    divC.style.flexDirection = "column";
+
     divC.classList.add(`column${i}`);
     divContent.appendChild(divC);
-    for (j = 0; j < 16; j++) {
+    for (j = 0; j < x; j++) {
       let divR = document.createElement("div");
       //   divR.textContent = `divJ${j}`;
       divR.style.border = "1px solid black";
+      divR.style.flexDirection = "column";
       divR.style.flexGrow = "1";
       divR.classList.add(`row${i}`);
       divR.classList.add("cell");
       divC.appendChild(divR);
     }
   }
+  changeColorOnMouseOver();
 }
 
-function mouseOver() {
+function changeColorOnMouseOver() {
   let cells = Array.from(document.querySelectorAll(".cell"));
-  console.log(cells);
+  //   console.log(cells);
   cells.forEach((cell) =>
     cell.addEventListener(
       "mouseover",
@@ -33,6 +36,26 @@ function mouseOver() {
   );
 }
 
-// function handleMouseOver(cell) {
-//     cell.style.backgroundColor = "black"
-// }
+function newGrid() {
+  let input = prompt("how many rows and columns do you want?", "16");
+  //   this if doesn't work. TODO find a way to distinguish number from string
+  if (!isNaN(input)) {
+    if (input > 100) {
+      alert("The grid is too big\ntry again");
+      newGrid();
+    }
+    let removeChild = divContent.hasChildNodes;
+
+    while (removeChild) {
+      if (divContent.lastChild.id == "newGrid") {
+        removeChild = false;
+        continue;
+      }
+      divContent.removeChild(divContent.lastChild);
+    }
+    createGrid(input);
+  } else {
+    alert("incorrect input\ntry again");
+    newGrid();
+  }
+}
