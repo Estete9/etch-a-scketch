79fb4d8c2aad5cc1b1bf29260607b87a51367219
Esteban <cepalacios@outlook.com>
2022-05-25 08:29:15 -0500

    Implement grid
    implemented a 16x16 div-grid with moseover event listener which changes
    to black background color when mouse over.

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 let divContent = document.querySelector(".content");
 createGrid();
+mouseOver();
 
 function createGrid() {
-  // create grid of 16 x 16
-  // make 2 loops, one for the rows and one for the columns
-  //   in the first loop, create a div for containing the rows
-  // in each second loop create a div, and change its display to flex so it adjusts one after the other
-  // find a size so the grid is 16 x 16
-  // append to de 'content' div
-
   for (i = 0; i < 16; i++) {
-    let divRow = document.createElement("div");
-    divRow.style.display = "flex";
-    divContent.appendChild(divRow);
+    let divC = document.createElement("div");
+    divC.style.flexGrow = "1";
+    divC.style.display = "flex";
+    divC.style.flexDirection = "column";
+    divC.classList.add(`column${i}`);
+    divContent.appendChild(divC);
     for (j = 0; j < 16; j++) {
-      let divSquare = document.createElement("div");
-      divSquare.classList.add("square");
-      divSquare.textContent = `$i: ${i} j: ${j}`;
-      divSquare.style.display = "inline-block";
-      divSquare.style.border = "2px solid black";
-      divSquare.style.flex = "1 1 auto";
-      divRow.appendChild(divSquare);
+      let divR = document.createElement("div");
+      //   divR.textContent = `divJ${j}`;
+      divR.style.border = "1px solid black";
+      divR.style.flexGrow = "1";
+      divR.classList.add(`row${i}`);
+      divR.classList.add("cell");
+      divC.appendChild(divR);
     }
   }
 }
+
+function mouseOver() {
+  let cells = Array.from(document.querySelectorAll(".cell"));
+  console.log(cells);
+  cells.forEach((cell) =>
+    cell.addEventListener(
+      "mouseover",
+      () => (cell.style.backgroundColor = "black")
+    )
+  );
+}
+
+// function handleMouseOver(cell) {
+//     cell.style.backgroundColor = "black"
+// }
